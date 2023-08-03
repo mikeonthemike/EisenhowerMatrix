599c49f95a7e03c6e60a8f14a428d3ac69338ca7
mikeonthemike <michael_spragg@hotmail.com>
2023-08-03 11:58:12 +0000

    Switched drop down selection of importance and urgency to sliders

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import React, { useState } from 'react';
 function App() {
   const [tasks, setTasks] = useState([]);
   const [taskInput, setTaskInput] = useState('');
-  const [taskCategory, setTaskCategory] = useState('');
+  const [importanceScore, setImportanceScore] = useState(5);
+  const [urgencyScore, setUrgencyScore] = useState(5);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const newTask = { task: taskInput, category: taskCategory };
+    const newTask = { task: taskInput, importance: importanceScore, urgency: urgencyScore };
     setTasks([...tasks, newTask]);
     setTaskInput('');
-    setTaskCategory('');
+    setImportanceScore(5);
+    setUrgencyScore(5);
   };
 
   return (
@@ -22,73 +24,73 @@ function App() {
           value={taskInput}
           onChange={(e) => setTaskInput(e.target.value)}
         />
-        <select
-          value={taskCategory}
-          onChange={(e) => setTaskCategory(e.target.value)}
-        >
-          <option value="" disabled>
-            Select a category
-          </option>
-          <option value="important-urgent">Important & Urgent</option>
-          <option value="important-not-urgent">Important & Not Urgent</option>
-          <option value="not-important-urgent">Not Important & Urgent</option>
-          <option value="not-important-not-urgent">Not Important & Not Urgent</option>
-        </select>
+        <div>
+          <label>Importance:</label>
+          <input
+            type="range"
+            min="0"
+            max="10"
+            value={importanceScore}
+            onChange={(e) => setImportanceScore(parseInt(e.target.value))}
+          />
+          <span>{importanceScore}</span>
+        </div>
+        <div>
+          <label>Urgency:</label>
+          <input
+            type="range"
+            min="0"
+            max="10"
+            value={urgencyScore}
+            onChange={(e) => setUrgencyScore(parseInt(e.target.value))}
+          />
+          <span>{urgencyScore}</span>
+        </div>
         <button type="submit">Add task</button>
       </form>
-      <div class="divTable" className="matrix">
-        <div class="divTableBody" >
-         
-            <div class="divTableRow">
-              <div class="divTableCell"
-                    className="quadrant important-urgent">
-                  <h3>Important & Urgent</h3>
-                  <ul>
-                    {tasks
-                      .filter((task) => task.category === 'important-urgent')
-                      .map((task, index) => (
-                        <li key={index}>{task.task}</li>
-                      ))}
-                  </ul>
-                </div>
-              
-              <div class="divTableCell" className="quadrant important-not-urgent">
-                  <h3>Important & Not Urgent</h3>
-                  <ul>
-                    {tasks
-                      .filter((task) => task.category === 'important-not-urgent')
-                      .map((task, index) => (
-                        <li key={index}>{task.task}</li>
-                      ))}
-                  </ul>
-                </div>
-            </div>
-            <div class="divTableRow">
-              <div class="divTableCell" className="quadrant not-important-urgent">
-                  <h3>Not Important & Urgent</h3>
-                  <ul>
-                    {tasks
-                      .filter((task) => task.category === 'not-important-urgent')
-                      .map((task, index) => (
-                        <li key={index}>{task.task}</li>
-                      ))}
-                  </ul>
-               
-              </div>
-              <div class="divTableCell" className="quadrant not-important-not-urgent">
-                  <h3>Not Important & Not Urgent</h3>
-                  <ul>
-                    {tasks
-                      .filter((task) => task.category === 'not-important-not-urgent')
-                      .map((task, index) => (
-                        <li key={index}>{task.task}</li>
-                      ))}
-                  </ul>
-                </div>
-            </div>
-          </div>
+      <div className="matrix">
+        <div className="quadrant important-urgent">
+          <h3>Important & Urgent</h3>
+          <ul>
+            {tasks
+              .filter((task) => task.importance >= 5 && task.urgency >= 5)
+              .map((task, index) => (
+                <li key={index}>{task.task}</li>
+              ))}
+          </ul>
+        </div>
+        <div className="quadrant important-not-urgent">
+          <h3>Important & Not Urgent</h3>
+          <ul>
+            {tasks
+              .filter((task) => task.importance >= 5 && task.urgency < 5)
+              .map((task, index) => (
+                <li key={index}>{task.task}</li>
+              ))}
+          </ul>
+        </div>
+        <div className="quadrant not-important-urgent">
+          <h3>Not Important & Urgent</h3>
+          <ul>
+            {tasks
+              .filter((task) => task.importance < 5 && task.urgency >= 5)
+              .map((task, index) => (
+                <li key={index}>{task.task}</li>
+              ))}
+          </ul>
+        </div>
+        <div className="quadrant not-important-not-urgent">
+          <h3>Not Important & Not Urgent</h3>
+          <ul>
+            {tasks
+              .filter((task) => task.importance < 5 && task.urgency < 5)
+              .map((task, index) => (
+                <li key={index}>{task.task}</li>
+              ))}
+          </ul>
         </div>
       </div>
+    </div>
   );
 }
 
